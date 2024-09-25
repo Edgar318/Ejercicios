422d5d8d8b31f649cd387c9d55540797d5b68150
A604\1DAW <edgarfer2205@gmail.com>
2024-09-25 20:34:27 +0200

    Ejercicio de pruebas 1

---
+++ ejercicios1/src/main/java/org/example/Ejercicios.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ejercicios {
+
+    public void actividad_inicial (){ System.out.println("¿Como te llamas?");
+
+        Scanner entrada = new Scanner (System.in);
+        //System.in
+
+        String nombre = entrada.next();
+
+        System.out.println("¿A que curso vas?");
+        String curso = entrada.next();
+
+        System.out.println("Hola " + nombre + " Bienvenido a " + curso);
+
+        System.out.println("Introduce un numero....");
+        int num1 = entrada.nextInt();
+
+        System.out.println("Introduce otro numero....");
+        int num2 = entrada.nextInt();
+
+        int resultado = num1 + num2;
+
+        System.out.println("El resultado de la suma es: " + resultado );
+
+    }
+}
+
+
+
